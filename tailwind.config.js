ad298a90fe48d88ed66addbdbbc34a90abfb8fe6
IhsanJati <ihsanjati127@gmail.com>
2026-05-07 18:18:08 +0700

    add nativewind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./App.tsx",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
